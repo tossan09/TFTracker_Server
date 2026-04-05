d97724956a4e7d540b25c7986c07bb2c4f6b13e7
tossan09 <jean.kazuki@gmail.com>
2026-04-05 14:28:08 -0300

    chore: reoganize program.cs & publish setup

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ using TFTDataTrackerApi.Security;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
-
 builder.Services.AddControllers();
+
 builder.Services.AddScoped<CompRepository>();
 builder.Services.AddScoped<MatchRepository>();
 builder.Services.AddScoped<SetRepository>();
@@ -19,24 +19,46 @@ builder.Services.AddScoped<StatsRepository>();
 builder.Services.AddScoped<DbContext>();
 
 builder.Services.AddScoped<TokenService>();
+
 builder.Services.AddJwtAuthentication(builder.Configuration);
 builder.Services.AddAuthorization();
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
-        policy => policy.WithOrigins("http://localhost:5173")
+        policy => policy.WithOrigins("http://localhost:5173", "http://tftracker.com.br:7032")
                         .AllowAnyHeader()
                         .AllowAnyMethod());
 });
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
 app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseHttpsRedirection();
-app.MapControllers();
-app.Run();
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+
+    if (builder.Environment.IsDevelopment())
+    {
+        endpoints.MapFallback(async ctx =>
+        {
+            ctx.Response.Redirect("https://localhost:3000/");
+            await Task.Yield();
+        });
+    }
+    else
+    {
+        endpoints.MapFallbackToFile("/index.html");
+    }
+});
+
+app.Run();
